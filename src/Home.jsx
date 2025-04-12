23e154826619c2aa4b4bce372645a6b18cc53666
Your Name <tharunkoti1999@gmail.com>
2025-04-12 21:20:00 +0530

    added a footer

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from './Navbar'
+import Footer from './Footer'
 import { Outlet } from 'react-router-dom'
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
     <>
       <Navbar />
       <Outlet />
+      <Footer />
     </>
   )
 }
